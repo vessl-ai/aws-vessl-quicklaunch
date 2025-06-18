3f7e24a9cec379ca410429028232ede391e14d23
moon <moon@vessl.ai>
2025-06-18 19:01:50 +0900

    gpu_operator version up

--- kube/gpu_operator.tf
+++ kube/gpu_operator.tf
@@ -10,11 +10,11 @@ locals {
 }
 
 resource "helm_release" "gpu_operator" {
-  repository = "https://nvidia.github.io/gpu-operator"
-  chart      = "gpu-operator"
-  name       = "gpu-operator"
-  namespace  = "kube-system"
+  repository       = "https://nvidia.github.io/gpu-operator"
+  chart            = "gpu-operator"
+  name             = "gpu-operator"
+  namespace        = "kube-system"
   create_namespace = true
-  version    = "1.10.0"
-  values     = [yamlencode(local.gpu_operator_helm_values)]
+  version          = "v24.3.0"
+  values           = [yamlencode(local.gpu_operator_helm_values)]
 }
