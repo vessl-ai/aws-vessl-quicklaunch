2cf2d567b21461f21223a28168711cdcc526d8a8
moon <moon@vessl.ai>
2025-06-18 19:02:41 +0900

    add eks_nodegroup_role to use ebs dynamic provisioning

--- iam.tf
+++ iam.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_policy" "external_dns_policy" {
   name        = "${var.stack_name}-external-dns-policy"
   description = "Policy for external-dns to manage Route 53 records"
-  
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "route53:ChangeResourceRecordSets",
         ]
         Resource = [
@@ -29,7 +29,7 @@ resource "aws_iam_policy" "external_dns_policy" {
 
 resource "aws_iam_role" "external_dns_role" {
   name = "${var.stack_name}-external-dns-role"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -55,30 +55,31 @@ resource "aws_iam_role_policy_attachment" "external_dns_policy_attachment" {
   policy_arn = aws_iam_policy.external_dns_policy.arn
 }
 
+
 resource "aws_iam_policy" "cluster_autoscaler_policy" {
   name        = "${var.stack_name}-cluster-autoscaler-policy"
   description = "Policy for cluster autoscaler to manage EC2 instances"
-  
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "autoscaling:SetDesiredCapacity",
           "autoscaling:TerminateInstanceInAutoScalingGroup",
         ]
         Resource = ["*"]
         Condition = {
           StringEquals = {
-            "aws:ResourceTag/k8s.io/cluster-autoscaler/enabled": "true",
-            "aws:ResourceTag/k8s.io/cluster-autoscaler/${var.stack_name}": "owned"
+            "aws:ResourceTag/k8s.io/cluster-autoscaler/enabled" : "true",
+            "aws:ResourceTag/k8s.io/cluster-autoscaler/${var.stack_name}" : "owned"
           }
         }
       },
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "autoscaling:DescribeAutoScalingGroups",
           "autoscaling:DescribeAutoScalingInstances",
           "autoscaling:DescribeLaunchConfigurations",
@@ -98,7 +99,7 @@ resource "aws_iam_policy" "cluster_autoscaler_policy" {
 
 resource "aws_iam_role" "cluster_autoscaler_role" {
   name = "${var.stack_name}-cluster-autoscaler-role"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -123,16 +124,17 @@ resource "aws_iam_role_policy_attachment" "cluster_autoscaler_policy_attachment"
   policy_arn = aws_iam_policy.cluster_autoscaler_policy.arn
 }
 
+
 resource "aws_iam_policy" "lb_controller_policy" {
   name        = "${var.stack_name}-lb-controller-policy"
   description = "Policy for AWS Load Balancer Controller to manage load balancers"
-  
+
   policy = file("${path.module}/resources/load-balancer-controller-iam-policy.json")
 }
 
 resource "aws_iam_role" "lb_controller_role" {
   name = "${var.stack_name}-lb-controller-role"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -156,4 +158,44 @@ resource "aws_iam_role" "lb_controller_role" {
 resource "aws_iam_role_policy_attachment" "lb_controller_policy_attachment" {
   role       = aws_iam_role.lb_controller_role.name
   policy_arn = aws_iam_policy.lb_controller_policy.arn
+}
+
+
+resource "aws_iam_role" "eks_nodegroup_role" {
+  name = "${var.stack_name}-eks-nodegroup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodegroup_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodegroup_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodegroup_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodegroup_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodegroup_ecr_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodegroup_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodegroup_ebs_csi_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.eks_nodegroup_role.name
 }
