5b8bf009bd5ef308029982023437ec1c8059343b
moon <moon@vessl.ai>
2025-06-20 01:31:46 +0900

    set force_destroy in route53

--- route53.tf
+++ route53.tf
@@ -1,7 +1,8 @@
 resource "aws_route53_zone" "primary" {
-  name    = var.cluster_domain_name
-  comment = "Primary Route 53 zone for ${var.cluster_domain_name}"
-  tags    = local.tags
+  name          = var.cluster_domain_name
+  comment       = "Primary Route 53 zone for ${var.cluster_domain_name}"
+  tags          = local.tags
+  force_destroy = true
 
   depends_on = [module.vpc]
 }
