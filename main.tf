72156aa33cf130fe9495e5c1c8ace180f582f86e
moon <moon@vessl.ai>
2025-06-23 18:53:53 +0900

    add helm version pinning

--- main.tf
+++ main.tf
@@ -4,7 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.17.0"
+    }
   }
+
   backend "s3" {
   }
 }
